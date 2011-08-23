69a6a1a6fae7f0a08c54b7b95ba9b0a63df43ef3
=Antti Nummiaho <=>
2011-08-23 15:29:23 +0300

    Some minor changes.
    Changed the main class in the build and bat files.
    Also, added a simple test menu to openmap.properties file.

--- eavdam.bat
+++ eavdam.bat
@@ -4,4 +4,4 @@ set OPENMAP_HOME=.
 
 set CLASSPATH=%OPENMAP_HOME%;%OPENMAP_HOME%\build\classes;%OPENMAP_HOME%\share;%OPENMAP_HOME%\lib\milStd2525_png.jar;%OPENMAP_HOME%\lib\openmap.jar;%OPENMAP_HOME%\lib\omsvg.jar;%OPENMAP_HOME%\lib\omj3d.jar;%OPENMAP_HOME%\lib\omcorba_vb.jar;
 
-java -Xmx64m -Ddebug.showprogress com.bbn.openmap.app.OpenMap
+java -Xmx64m -Ddebug.showprogress dk.frv.eavdam.app.MainApp
