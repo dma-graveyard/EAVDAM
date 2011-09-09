e4e4a991151a07a4bb401a8aa5792700da344890
Antti Nummiaho <Antti.Nummiaho@vtt.fi>
2011-09-09 12:34:17 +0300

    Classes for stations of different statuses

--- src/main/java/dk/frv/eavdam/data/EAVDAMData.java
+++ src/main/java/dk/frv/eavdam/data/EAVDAMData.java
@@ -6,8 +6,14 @@ import java.util.List;
 public class EAVDAMData {
 
 	private EAVDAMUser user;
-	private List<AISFixedStationData> stations = new ArrayList<AISFixedStationData>();
-
+	
+	private List<AISFixedStationData> stations = new ArrayList<AISFixedStationData>();  // TO BE REMOVED
+	
+	private List<ActiveStation> activeStations = new ArrayList<ActiveStation>();
+    private List<OtherUserStations> otherUsersStations = new ArrayList<OtherUserStations>();
+    private List<Simulation> simulatedStations = new ArrayList<Simulation>();
+    private List<AISFixedStationData> oldStations = new ArrayList<AISFixedStationData>();
+    
 	public EAVDAMUser getUser() {
 		return user;
 	}
@@ -16,25 +22,49 @@ public class EAVDAMData {
 		this.user = user;
 	}
 
+    public List<ActiveStation> getActiveStations() {
+        return activeStations;
+    }
+    
+    public void setActiveStations(List<ActiveStation> activeStations) {
+        this.activeStations = activeStations;
+    }
+
+    public List<OtherUserStations> getOtherUsersStations() {
+        return otherUsersStations;
+    }
+    
+    public void setOtherUsersStations(List<OtherUserStations> otherUsersStations) {
+        this.otherUsersStations = otherUsersStations;
+    }    
+
+    public List<Simulation> getSimulatedStations() {
+        return simulatedStations;
+    }
+    
+    public void setSimulatedStations(List<Simulation> simulatedStations) {
+        this.simulatedStations = simulatedStations;
+    }    
+    
+    public List<AISFixedStationData> getOldStations() {
+        return oldStations;
+    }
+    
+    public void setOldStations(List<AISFixedStationData> oldStations) {
+        this.oldStations = oldStations;
+    }
+    
+    // TO BE REMOVED -->
 	public AISFixedStationData[] getStations() {
 		return stations.toArray(new AISFixedStationData[stations.size()]);
 	}
-
-	public void addStation(AISFixedStationData station) {
-		if (station != null) {
-			this.stations.add(station);
-		}
-	}
-
+	
 	public void setStations(List<AISFixedStationData> stations) {
 		this.stations.clear();
 		if (stations != null) {
 			this.stations.addAll(stations);
 		}
 	}
+	// <-- TO BE REMOVED
 
-	@Override
-	public String toString() {
-		return "EAVDAMData [user=" + user + ", stations=" + stations + "]";
-	}
 }
