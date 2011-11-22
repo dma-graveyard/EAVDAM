51a847a338e730ee979a7a2462b865062e944e8d
Antti Nummiaho <Antti.Nummiaho@vtt.fi>
2011-11-22 16:10:28 +0200

    Small fix.

--- src/main/java/dk/frv/eavdam/data/AISStation.java
+++ src/main/java/dk/frv/eavdam/data/AISStation.java
@@ -7,13 +7,17 @@ public class AISStation {
 
 	private String organizationName;
 	private String stationName;
+	private Double latitude;
+	private Double longitude;
 	// should have more information?
 	
 	public AISStation() {}
 	
-	public AISStation(String organizationName, String stationName) {
+	public AISStation(String organizationName, String stationName, Double latitude, Double longitude) {
 		this.organizationName = organizationName;
 		this.stationName = stationName;
+		this.latitude = latitude;
+		this.longitude = longitude;
 	}
 
 	public String getOrganizationName() {
@@ -23,5 +27,29 @@ public class AISStation {
 	public void setOrganizationName(String organizationName) {
 		this.organizationName = organizationName;
 	}
+	
+	public String getStationName() {
+		return stationName;
+	}
+
+	public void setStationName(String stationName) {
+		this.stationName = stationName;
+	}	
+	
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}		
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}		
 	
 }
